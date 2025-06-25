1d8485ad16590cbbc8c4ead4deec61589e4e8f18
Werner Fouché <werner.fouche@gmail.com>
2025-06-25 07:07:24 +0200

    Used Scanner class to read input

--- src/main/java/dev/jbang/util/ConsoleInputReadTask.java
+++ src/main/java/dev/jbang/util/ConsoleInputReadTask.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 import java.util.concurrent.Callable;
 
 public class ConsoleInputReadTask implements Callable<String> {
@@ -14,19 +15,7 @@ public class ConsoleInputReadTask implements Callable<String> {
 	}
 
 	public String call() throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(in));
-		String input;
-		do {
-			try {
-				// wait until we have data to complete a readLine()
-				while (!br.ready()) {
-					Thread.sleep(200);
-				}
-				input = br.readLine();
-			} catch (InterruptedException e) {
-				return null;
-			}
-		} while ("".equals(input));
-		return input;
+		Scanner sc = new Scanner(in);
+		return sc.nextLine();
 	}
 }
