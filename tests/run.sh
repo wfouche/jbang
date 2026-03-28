6866370f44aee6cfbe6d17a716215d728f9a868e
Werner Fouché <werner.fouche@gmail.com>
2026-03-28 14:36:36 +0200

    Updated tests

--- tests/run.sh
+++ tests/run.sh
@@ -1,9 +1,14 @@
+set  SDKMAN_DIR="$HOME/.sdkman"
+[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
+sdk use java 21.0.10-tem
+
 #../build/install/jbang/bin/jbang run cli --version
 #../build/install/jbang/bin/jbang run h
 #../build/install/jbang/bin/jbang run h:1.0.1
 
-../build/install/jbang/bin/jbang run hw
+#../build/install/jbang/bin/jbang run hw
 ../build/install/jbang/bin/jbang run hw2
 
-../build/install/jbang/bin/jbang run camel
-../build/install/jbang/bin/jbang run camel:4.17.0
+#../build/install/jbang/bin/jbang run camel version
+../build/install/jbang/bin/jbang run camel:4.17.0 version
+
