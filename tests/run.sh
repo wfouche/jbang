701029170cd99457959581274484c3d79dfd3357
Werner Fouché <werner.fouche@gmail.com>
2026-03-28 14:25:51 +0200

    feat: add camel support

--- tests/run.sh
+++ tests/run.sh
@@ -1,5 +1,9 @@
 #../build/install/jbang/bin/jbang run cli --version
 #../build/install/jbang/bin/jbang run h
 #../build/install/jbang/bin/jbang run h:1.0.1
+
 ../build/install/jbang/bin/jbang run hw
 ../build/install/jbang/bin/jbang run hw2
+
+../build/install/jbang/bin/jbang run camel
+../build/install/jbang/bin/jbang run camel:4.17.0
