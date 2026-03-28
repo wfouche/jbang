4193420acd763bd01c78c77bcd3d404c6eee70f7
Werner Fouché <werner.fouche@gmail.com>
2026-03-28 14:53:52 +0200

    Formatting cleanup

--- tests/run.sh
+++ tests/run.sh
@@ -2,13 +2,20 @@ set  SDKMAN_DIR="$HOME/.sdkman"
 [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk use java 21.0.10-tem
 
-#../build/install/jbang/bin/jbang run cli --version
-#../build/install/jbang/bin/jbang run h
-#../build/install/jbang/bin/jbang run h:1.0.1
+export JBANG=~/IdeaProjects/jbang/build/install/jbang/bin/jbang
+# $JBANG run cli --version
+# $JBANG run h
+# $JBANG run h:1.0.1
 
-#../build/install/jbang/bin/jbang run hw
-../build/install/jbang/bin/jbang run hw2
+echo ""
+echo "Hello world tests"
+echo ""
+$JBANG run hw
+$JBANG run hw2
 
-#../build/install/jbang/bin/jbang run camel version
-../build/install/jbang/bin/jbang run camel:4.17.0 version
+echo ""
+echo "Camel tests"
+echo ""
+$JBANG run camel version
+$JBANG run camel:4.17.0 version
 
